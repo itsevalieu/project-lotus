d59a6e7a23a624e7368753d730804fb01292c290
itsevalieu <itsevalieu@gmail.com>
2018-07-05 23:53:41 -0700

    Fixed paths for Footer, Header, Siderbar

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Button.css';
+
+class Button extends Component {
+	render() {
+		return (
+			<button></button>
+        );
+	}
+};
+
+export default Button;
