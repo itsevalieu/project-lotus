91a8c31d622531f8ac2234e27ed07d310cd73c73
Eva Lieu <itsevalieu@gmail.com>
2018-08-02 19:52:52 -0700

    Adjusting padding size to larger

--- src/containers/Landing/components/Billing/Billing.jsx
+++ src/containers/Landing/components/Billing/Billing.jsx
@@ -10,33 +10,33 @@ class Billing extends Component {
 	render() {
 		return(
 			<div className='billing'>
-				<div><h3>CHOOSE YOUR BILLING PLAN</h3></div>
+				<div><h2>Choose Your Billing Plan</h2></div>
 				<div className='billing-buttons'>
 					<button>
 						<img src={icon1} alt=''/>
 						<div className='billing-buttons-text'>
-							<h2>Monthly</h2>
-							<p>Pay your subscription plan every month for 12 payments total.</p>
+							<h3>Monthly</h3>
+							<p>Pay your plan every month for 12 payments total.</p>
 						</div>
 					</button>
 					<button>
 						<img src={icon3} alt=''/>
 						<div className='billing-buttons-text'>
-							<h2>Quarterly</h2>
+							<h3>Quarterly</h3>
 							<p>Pay your plan every three months for 4 payments total.</p>
 						</div>
 					</button>
 					<button>
 						<img src={icon6} alt=''/>
 						<div className='billing-buttons-text'>
-							<h2>Semi-Yearly</h2>
+							<h3>Semi-Yearly</h3>
 							<p>Pay your plan every six months for 2 payments total.</p>				
 						</div>
 					</button>
 					<button>
 						<img src={icon12} alt=''/>
 						<div className='billing-buttons-text'>
-							<h2>Yearly</h2>
+							<h3>Yearly</h3>
 							<p>Make a one-time payment for a whole year!</p>
 						</div>
 					</button>
