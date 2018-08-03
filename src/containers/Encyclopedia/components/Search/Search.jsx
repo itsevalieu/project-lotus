0dc42e71e537fb69320ccf18339687f3c64cfd50
Eva Lieu <itsevalieu@gmail.com>
2018-08-02 19:54:04 -0700

    Centering text for Search component, increase padding

--- src/containers/Encyclopedia/components/Search/Search.jsx
+++ src/containers/Encyclopedia/components/Search/Search.jsx
@@ -6,9 +6,9 @@ class Search extends Component {
 	render() {
 		return(
 			<div id='search' className='search'>
-				<h3>EXPLORE THE WORLD OF TEA</h3>
+				<h2>Explore the World of Tea</h2>
 				<form onSubmit={this.props.onSubmitAction}>
-					<p>Build your tea knowledge using our encyclopedia. Add the teas you want to your tea box subscription.</p>
+					<p>Build your tea knowledge using our tea and herb encyclopedia. <br/> Try searching for different types of green, black, white, or oolong tea!</p>
 					<span>
 						<input 
 							type='search' 
@@ -16,7 +16,7 @@ class Search extends Component {
 							value={this.props.query}
 							onChange={this.props.onChangeValue} 
 							id='searchbar' 
-							placeholder='Search teas, herbs, or health benefits...'
+							placeholder='Search teas or herbs...'
 						/>
 						<button type='submit'>SEARCH</button>
 					</span>
