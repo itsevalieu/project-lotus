7d2b5e40ae74c1ad194b54f289c882c222dbb622
Eva Lieu <itsevalieu@gmail.com>
2018-08-18 00:46:51 -0700

    Adding pre-state for loading results

--- src/containers/Encyclopedia/components/Results/Results.jsx
+++ src/containers/Encyclopedia/components/Results/Results.jsx
@@ -4,6 +4,9 @@ import './Results.css';
 class Results extends Component {
 	
 	render() {
+		if(!this.props.results.length) {
+			return <div style={{textAlign: 'center', background: '#eee', height: '100px', padding: '25px'}}>Did you know all traditional teas come from a single plant? It's called camellia sinesis!</div>;
+		}
 		return(
 			<div className='results'>
 				<div><p>Search results for: <span id='searchterm'>{this.props.query}</span></p></div>
