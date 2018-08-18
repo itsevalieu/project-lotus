d55ed3966deefcc2f0f5f2806810bcb646f4cb88
Eva Lieu <itsevalieu@gmail.com>
2018-08-18 00:47:58 -0700

    Adding quick search links and styling

--- src/containers/Encyclopedia/components/Search/Search.jsx
+++ src/containers/Encyclopedia/components/Search/Search.jsx
@@ -21,6 +21,16 @@ class Search extends Component {
 						<button type='submit'>SEARCH</button>
 					</span>
 				</form>
+				<div id='quicksearch'>
+				<p>Quick Search:</p>
+					<a href='' className='quicksearch' data-value='black' onClick={this.props.handleClick}>Black</a>
+					<a href='' className='quicksearch' data-value='green' onClick={this.props.handleClick}>Green</a>
+					<a href='' className='quicksearch' data-value='white' onClick={this.props.handleClick}>White</a>
+					<a href='' className='quicksearch' data-value='oolong' onClick={this.props.handleClick}>Oolong</a>
+					<a href='' className='quicksearch' data-value='puerh' onClick={this.props.handleClick}>Pu-erh</a> 
+					<a href='' className='quicksearch' data-value='herbal' onClick={this.props.handleClick}>Herbal</a>
+					<a href='' className='quicksearch' data-value='fruitblend' onClick={this.props.handleClick}>Fruit Blend</a> 
+				</div>
 			</div>
 		);
 	}
